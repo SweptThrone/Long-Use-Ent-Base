2db3dcf01957905a05f36a97ec52f558beca8bc4
SweptThrone <ejvogt5@gmail.com>
2021-01-18 20:26:34 -0600

    networked settings vars, new stuff...
    new function ENT:OnUseCancel( ply ) to be called when ply lets go of the entity before it's done
    new table ENT.PartialUses to be called at the indexed points, with the entity as a parameter.  read this file for a brief tutorial

--- LongUseEntBase/lua/entities/long_use_base.lua
+++ LongUseEntBase/lua/entities/long_use_base.lua
@@ -1,23 +1,59 @@
 AddCSLuaFile()
 
 ENT.Type = "anim"
---ENT.Base = "base_gmodentity"
 ENT.PrintName = "Progress Usable"
 ENT.Author = "SweptThrone"
 ENT.Spawnable = true
 ENT.AdminSpawnable = false
-ENT.ProgPerTick = 0
+ENT.AutomaticFrameAdvance = true
+ENT.ProgPerTick = 0 -- used internally, this should not be changed
 ENT.TimeToUse = ENT.TimeToUse or 1.5 -- the user can edit this
-ENT.DrawKeyPrompt = true -- the user can edit this
-ENT.DrawProgress = true -- the user can edit this
---ENT.SingleUser = false -- the user can edit this
+ENT.LastProg = -1 -- used internally, this should not be changed
+--ENT.DrawKeyPrompt = true -- deprecated, use self:SetDrawKeyPrompt( b )
+--ENT.DrawProgress = true -- deprecated, use self:SetProgress( b )
+
+ENT.PartialUses = {}
+--[[ this is an example of a filled-in PartialUses table
+	if your TimeToUse is too short,
+	some intermittent steps may not get called.
+	for example, this very table on a TimeToUse = 0.5 or even 1
+	only calls 0, 60, and 99.
+	i'm sure if you did any amount of research,
+	you could get this down to a science.
+	TimeToUse = 1.5 (default) works fine with this table.
+
+ENT.PartialUses = {
+	[0] = function( ent )
+		ent:EmitSound( "buttons/blip1.wav", 75, 38 )
+	end,
+	[20] = function( ent )
+		ent:EmitSound( "buttons/blip1.wav", 75, 50 )
+	end,
+	[40] = function( ent )
+		ent:EmitSound( "buttons/blip1.wav", 75, 63 )
+	end,
+	[60] = function( ent )
+		ent:EmitSound( "buttons/blip1.wav", 75, 75 )
+	end,
+	[80] = function( ent )
+		ent:EmitSound( "buttons/blip1.wav", 75, 88 )
+	end,
+	[99] = function( ent )
+		ent:EmitSound( "buttons/blip1.wav", 75, 99 )
+	end
+}
+]]
 
 function ENT:SetupDataTables()
 	self:NetworkVar( "Float", 0, "Progress" )
 	self:NetworkVar( "Entity", 0, "User" )
+	self:NetworkVar( "Bool", 0, "DrawKeyPrompt" )
+	self:NetworkVar( "Bool", 1, "DrawProgress" )
 
 	if SERVER then
-		self:SetProgress( -1 )
+		self:SetProgress( -1.0 )
+		self:SetDrawKeyPrompt( true )
+		self:SetDrawProgress( true )
 		self.ProgPerTick = 1 / ( self.TimeToUse * ( 1 / FrameTime() ) / 100 )
 	end
 end
@@ -38,7 +74,7 @@ if SERVER then
 	end
 
 	function ENT:Use( ply, act, typ )
-		if --[[self.SingleUser and]] IsValid( self:GetUser() ) and self:GetUser() != ply then return end
+		if IsValid( self:GetUser() ) and self:GetUser() != ply then return end
 
 		self:SetUser( ply )
 		if !ply:KeyDownLast( IN_USE ) then
@@ -58,6 +94,11 @@ if SERVER then
 			self:SetProgress( -1 )
 			self:SetUser( nil )
 		end
+
+		if self.PartialUses[ self:GetProgress() ] != nil then
+			self.PartialUses[ self:GetProgress() ]( self )
+		end
+
 	end
 
 	function ENT:OnUseFinish( ply )
@@ -68,11 +109,42 @@ if SERVER then
 		self:EmitSound( "items/ammopickup.wav" )
 	end
 
+	function ENT:OnUseCancel( ply )
+		self:EmitSound( "buttons/button10.wav" )
+	end
+
 	function ENT:OnRemove()
 		
 	end
 
 	function ENT:Think()
+
+		if IsValid( self:GetUser() ) then
+            if !IsValid( self:GetUser():GetUseEntity() ) and self:GetUser():GetUseEntity() != self then
+                self:SetUser( nil )
+                self:SetProgress( -1 )
+            end
+        end
+
+		if self:GetProgress() == self.LastProg and self:GetProgress() != -1 then
+			self:OnUseCancel( self:GetUser() )
+            self:SetUser( nil )
+            self:SetProgress( -1 )
+		end
+
+		self.LastProg = self:GetProgress()
+
+		--[[
+			usually you'd just use CurTime() here,
+			but on 66-tick servers, it's extremely unreliable.
+			sometimes it will cancel using even if you're holding it.
+			this is a bit of a fix, but it runs this entity
+			at 33-tick i think...
+		]]--
+		self:NextThink( CurTime() + 0.03 )
+
+		return true
+
 	end
 
 end
